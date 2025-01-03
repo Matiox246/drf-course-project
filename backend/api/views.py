0ecb0da068baab9a2550c432b53aafe6a670f6d5
Matin <mohamad13840208@gmail.com>
2025-01-03 20:23:37 +0330

    last update for permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,26 +6,29 @@ from django.contrib.auth.models import User
 
 from .serializers import ArticleSerializer, UserSerializer
 from blog.models import Article
+from .permissions import IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly
 # Create your views here.
 
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsStaffOrReadOnly, )
 
 
 class ArticleDeatil(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
     # lookup_field = "slug"
 
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUserOrStaffReadOnly,)
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUserOrStaffReadOnly,)
